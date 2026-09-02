d871262ffa24bdf77791c99dc25d0d7b84e971b9
RavenRT02 <praveenkumart261@gmail.com>
2026-09-02 14:17:11 +0530

    config model comments and client.py os.getenv fix

--- config.py
+++ config.py
@@ -2,13 +2,17 @@
 
 LLM_PROVIDER = "local"
 LLM_MODEL = 'meta-llama/Meta-Llama-3.1-8B-Instruct'
+BASE_URL = None 
 
-
-# API calls
+# API calls ( 2 example sets - tested models and url as of 02/09/2026 )
 
 # LLM_PROVIDER = "api"
-# LLM_MODEL = ""
-BASE_URL = None        # Replace with url for providers other than openai               
+# LLM_MODEL = "gpt-4.1-mini"
+# BASE_URL = None                      # Replace with url for providers other than openai       
+
+# LLM_PROVIDER = "api"
+# LLM_MODEL = "gemini-3.6-flash"
+# BASE_URL = "https://generativelanguage.googleapis.com/v1beta/openai/"
 
 
 # Token constants
